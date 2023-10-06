b8657bb6bf4c99dfb310a172ffad6fbaf409a144
Milind Nair <milindnair261003@gmail.com>
2023-10-06 11:34:11 +0530

    feat:Feedback,SidePanel,Doctor profile,Contact Us

--- frontend/src/pages/Doctors/DoctorAbout.jsx
+++ frontend/src/pages/Doctors/DoctorAbout.jsx
@@ -3,32 +3,51 @@ import { formatDate } from '../../utils/formatDate'
 const DoctorAbout = () => {
     return (
         <div>
-        <div>
-            <h3 className='text-[20px] leading-[30px] text-headingColor font-semibold flex items-center gap-2'>About of <span className='text-irisBlueColor font-bold text-[24px] leading-9 '>Karthikeya Mishra</span></h3>
-            <p className="text__para">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, reiciendis. Voluptas sequi eveniet quidem accusantium voluptatibus quae vero distinctio. Illo ullam nobis atque exercitationem aut alias laborum consequuntur eius natus.</p>
-        </div>
-        <div className='mt-12'>
-            <h3 className='text-[20px] leading-[30px] text-headingColor font-semibold'>Education</h3>
-            <ul className='pt-4 md:p-5'>
-                <li className='flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]'>
-                    <div>
-                        <span className='text-irisBlueColor text-[15px] leading-6 font-semibold'>{formatDate('12-04-2000')} - {formatDate('12-04-2002')}</span>
-                        <p className='text-[16px] leading-6 font-medium text-textColor'>PHD in surgeon</p>
-                    </div>
+            <div>
+                <h3 className='text-[20px] leading-[30px] text-headingColor font-semibold flex items-center gap-2'>About of <span className='text-irisBlueColor font-bold text-[24px] leading-9 '>Karthikeya Mishra</span></h3>
+                <p className="text__para">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, reiciendis. Voluptas sequi eveniet quidem accusantium voluptatibus quae vero distinctio. Illo ullam nobis atque exercitationem aut alias laborum consequuntur eius natus.</p>
+            </div>
+            <div className='mt-12'>
+                <h3 className='text-[20px] leading-[30px] text-headingColor font-semibold'>Education</h3>
+                <ul className='pt-4 md:p-5'>
+                    <li className='flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]'>
+                        <div>
+                            <span className='text-irisBlueColor text-[15px] leading-6 font-semibold'>{formatDate('12-04-2000')} - {formatDate('12-04-2002')}</span>
+                            <p className='text-[16px] leading-6 font-medium text-textColor'>PHD in surgeon</p>
+                        </div>
+                        <p className='text-[16px] leading-5 font-medium text-textColor'>New Apollo Hospital,New York</p>
+
+                    </li>
+                    <li className='flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]'>
+                        <div>
+                            <span className='text-irisBlueColor text-[15px] leading-6 font-semibold'>{formatDate('12-04-2010')} - {formatDate('12-04-2012')}</span>
+                            <p className='text-[16px] leading-6 font-medium text-textColor'>PHD in surgeon</p>
+                        </div>
+                        <p className='text-[16px] leading-5 font-medium text-textColor'>New Apollo Hospital,New York</p>
+
+                    </li>
+
+                </ul>
+            </div>
+            <div className='mt-12'>
+            <h3 className='text-[20px] leading-[30px] text-headingColor font-semibold'>Experience</h3>
+            <ul className="grid sm:grid-cols-2 gap-[30px] pt-4 md:p-5">
+                <li className='p-4 rounded bg-[#fff9ea]'>
+                    <span className="text-yellowColor text-[15px] leading-6 font-semibold">
+                        {formatDate('12-04-2000')} - {formatDate('12-04-2002')}
+                    </span>
+                    <p className='text-[16px] leading-6 font-medium text-textColor'>Sr. Surgeon</p>
                     <p className='text-[16px] leading-5 font-medium text-textColor'>New Apollo Hospital,New York</p>
-
                 </li>
-                <li className='flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]'>
-                    <div>
-                        <span className='text-irisBlueColor text-[15px] leading-6 font-semibold'>{formatDate('12-04-2010')} - {formatDate('12-04-2012')}</span>
-                        <p className='text-[16px] leading-6 font-medium text-textColor'>PHD in surgeon</p>
-                    </div>
+                <li className='p-4 rounded bg-[#fff9ea]'>
+                    <span className="text-yellowColor text-[15px] leading-6 font-semibold">
+                        {formatDate('12-04-2000')} - {formatDate('12-04-2002')}
+                    </span>
+                    <p className='text-[16px] leading-6 font-medium text-textColor'>Sr. Surgeon</p>
                     <p className='text-[16px] leading-5 font-medium text-textColor'>New Apollo Hospital,New York</p>
-
                 </li>
-
             </ul>
-        </div>
+            </div>
         </div>
     )
 }
---
+++ frontend/src/pages/Doctors/FeedbackForm.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+import { AiFillStar } from 'react-icons/ai';
+
+const FeedbackForm = () => {
+    const [rating, setRating] = useState(0)
+    const [hover, setHover] = useState(0)
+    const [reviewText, setReviewText] = useState('')
+    
+    const handleSubmitReview = async(e) => {
+        e.preventDefault()
+        console.log(rating, reviewText)
+    };
+
+
+    return (
+        <form action="">
+            <div>
+                <h3 className="text-headingColor text-[16px] leading-6 font-semibold mb-4">
+                    How much would you rate your overall experience?
+                </h3>
+
+                <div>
+                    {[...Array(5).keys()].map((_, index) => {
+                        index += 1;
+                        return (
+                            <button
+                                key={index}
+                                type='button'
+                                className={`${index <= ((rating && hover) || hover) ? "text-yellowColor" : "text-gray-400"} bg-transparent border-none outline-none text-[22px] cursor-pointer`}
+                                onMouseEnter={() => setHover(index)}
+                                nMouseLeave={() => setHover(rating)}
+                                onDoubleClick={() => { setHover(0); setRating(0) }}
+                                onClick={() => setRating(index)}
+                            >
+                                <span>
+                                    <AiFillStar></AiFillStar>
+                                </span>
+                            </button>)
+                    })}
+                </div>
+            </div>
+
+            <div className="mt-[30px]">
+            <h3 className="text-headingColor text-[16px] leading-6 font-semibold mb-4">
+                    Share your feedback or suggestions
+                </h3>
+                <textarea name="" id="" className='border border-solid border-[#00664434] focus:outline outline-primaryColor w-full px-4 py-3 rounded-md' placeholder='Write your message' rows="5" onChange={()=>setReviewText(e.target.value)}></textarea>
+            </div>
+
+            <button type='submit' className='btn' onClick={handleSubmitReview}>Submit Feedback</button>
+        </form>
+
+    )
+}
+
+export default FeedbackForm
